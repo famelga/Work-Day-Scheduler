e5bfe56270229df2eaa047581da1819a645c4b3c
Fayven <fayven.amelga@gmail.com>
2023-01-06 22:01:08 -0500

    Add pseudocode for functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+// dayjs().isSame('hours')
 // $(".lead").wrap("wait");
 // $(".container-lg").wrap("body");
-var leadEl = document.querySelector(".lead");
+
+// var leadEl = document.getElementById("currentDay");
+var leadEl = $('#currentDay');
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -14,31 +17,46 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  document.querySelector(".btn saveBtn col-2 col-md-1").addEventListener("click", function() {
-
-  })
+  // document.querySelector(".btn saveBtn col-2 col-md-1").addEventListener("click", function() {
+// Dom traversal children, div to ID ("this").attri("ID")
+  // })
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  // loop
+  // if id hour > hh gray
+  // else if id hour === hh red
+  // else id hour < dayjs().isSame('hours') green
+  // <time>
+  // query isbefore same compare
+  
+  var hour = document.getElementById("hour");
+  console.log("hour"); 
+
+// function timeBlock() {
+//   for (i=0; i<  )
+// }
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
   
-  var now = dayjs().format('hh:mm:ss');
+  var now = dayjs().format('MMMM DD, YYYY');
   $('#currentDay').text(now);
   console.log(now);
 
-  // var countDown = setInterval(currentTime, 1000);
-  // function currentTime() {
-  //   now--;
-  //   leadEl.textContent = now;
-  //   currentTime();
-  }
+  
 });
 
+// var countDown = setInterval(currentTime, 1000);
+//   function currentTime() {
+//     now--;
+//     $('#currentDay').text(now);
+//     currentTime();
+//   }
