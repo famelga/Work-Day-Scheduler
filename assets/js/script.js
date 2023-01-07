326edabf790e82ed1423337da4f8f547826dea9c
Fayven <fayven.amelga@gmail.com>
2023-01-06 19:50:43 -0500

    Link css and js to html properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// $(".lead").wrap("wait");
+// $(".container-lg").wrap("body");
+var leadEl = document.querySelector(".lead");
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,6 +14,10 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  document.querySelector(".btn saveBtn col-2 col-md-1").addEventListener("click", function() {
+
+  })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +29,16 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+  var now = dayjs().format('hh:mm:ss');
+  $('#currentDay').text(now);
+  console.log(now);
+
+  // var countDown = setInterval(currentTime, 1000);
+  // function currentTime() {
+  //   now--;
+  //   leadEl.textContent = now;
+  //   currentTime();
+  }
 });
+
